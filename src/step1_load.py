2d6179074062bb11947ea4e4c1fe03c40271e1d3
Rijul Poudel <rijulpoudel72@gmail.com>
2026-05-20 21:17:36 -0500

    completed step1 -> loading file

--- src/step1_load.py
+++ src/step1_load.py
@@ -23,7 +23,43 @@ def load_images(path1, path2):
     return img1_color, img2_color, img1_gray, img2_gray
 
 
-load_images('images/img1.jpg', 'images/img2.jpg')
+
+
+def display_images(img1_color, img2_color):
+    img1_rgb = cv2.cvtColor(img1_color, cv2.COLOR_BGR2RGB) 
+    #since matplotlib need rgb, we convert bgr to rgb
+    img2_rgb = cv2.cvtColor(img2_color, cv2.COLOR_BGR2RGB)
+
+    fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+    #subplots creates a grid. 1 represents rows, so it will have 1 row and 2 columns (basically for the 2 images) 
+    #figsize creates the dimensions of the object 12 x 5
+    #fig is the overall figure container and axes is an array of two plot areas, one for each image. Can access them as axes[0] and axes[1]
+
+    axes[0].imshow(img1_rgb) #places the img1_rgb inside the left box. nothing displays yet since we havent don't plt.show() yet.
+    axes[0].set_title("Image 1 (Base)", fontsize=14) #adds ttile above left subplot
+    axes[0].axis('off')#hides the x and y axis ticks and numbers around the image. since images aren't graphs, we don't need it.
+    axes[1].imshow(img2_rgb)
+    axes[1].set_title("Image 2 (To Align)", fontsize=14)
+    axes[1].axis('off')
+
+
+    plt.suptitle("Step 1: Loaded Images", fontsize=16, fontweight='bold') #it adds one big title above both subplots combined
+
+    plt.tight_layout() #adjusts the spacing between subplots so nothing overlaps. fixes the spacing issues
+
+    plt.savefig("output/step1_loaded.jpg", dpi=150, bbox_inches='tight') #save to output folder as step1_loaded.jpg, dpi 150 and bbox_inches crops any extra whitespace 
+
+    plt.show()
+
+    print("saved: output/step1_loaded.jpg")
+
+
+
+if __name__ == "__main__":
+    img1c, img2c, img1g, img2g = load_images("images/img1.jpg", "images/img2.jpg")
+    display_images(img1c, img2c)
+
+
 
 
 
